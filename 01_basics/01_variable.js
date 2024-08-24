a44186ac45cec6fd882996f3976c5c2e0cf9052f
Pintu9040 <pintuojha61@gmail.com>
2024-08-24 16:11:22 +0000

    container confic done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,8 @@
+const accountId = 14445853
+let accountEmail = "pintu@mail.com"
+var accountPassword = "123456"
+accountCity = "balasore"
+
+// accountId = 2 //not allowed
+
+console.log(accountId);
