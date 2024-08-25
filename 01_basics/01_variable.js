d82266672a29c1c51202153bb18634ca0295e775
Pintu9040 <pintuojha61@gmail.com>
2024-08-25 13:59:52 +0000

    variable

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -2,7 +2,19 @@ const accountId = 14445853
 let accountEmail = "pintu@mail.com"
 var accountPassword = "123456"
 accountCity = "balasore"
+let accountState;
 
 // accountId = 2 //not allowed
 
+accountEmail = "pintu@ht.com"
+accountPassword = "1234512"
+accountCity = "soro"
+
 console.log(accountId);
+
+/*
+prefer not to use var 
+because of issue in block scope and function scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
