4112dff7cdab2e9e2362c7f4695050ce4239b559
Pintu9040 <pintuojha61@gmail.com>
2024-08-26 07:31:50 +0000

    conversion of operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+//let score = "33"
+//let score = "33abc"
+//let score = null
+//let score = undefined
+let score = "pintu"
+
+//console.log(typeof score);
+//console.log (typeof(score));
+
+let valueNumber = Number(score);
+//console.log(typeof valueNumber );
+//console.log(valueNumber)
+
+
+//"33" => 33
+//"33abc"=>NaN
+// true => 1 , fulse = 0 .
+
+let isLoggedIn = "pintu"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true , 0 => false
+// "" => false
+// "pintu" => true
+
+
+let someNumber = 33 ;
+let stringSomeNumber = String(someNumber);
+console.log(stringSomeNumber);
+console.log(typeof stringSomeNumber);
+
