bc03fb0d3600fd01f8e784e25cb058036b07c34f
Pintu9040 <pintuojha61@gmail.com>
2024-08-26 06:52:02 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,26 @@
+"use strict" //treat all js code as newer version
+
+//alert( 3 + 3 ) //this is not good practice
+
+console.log(3 
+    + 3) //code readability should be high
+
+    console.log("pintu")
+
+let name = "pintu"
+let age = 18
+
+//datatypes
+// 1 - number
+// 2 - bigint
+// 3 - string => ""
+// 4 - boolean => true/fulse
+// 5 - null  => standalone value
+// 6 - undefined => 
+// 7 - symbol => unique
+
+
+// object
+
+console.log(typeof "pintu");
+console.log(typeof null);  // object
