ec6f0d898d159d1c9e7bd8228ae05cdf068695e3
Pintu9040 <pintuojha61@gmail.com>
2024-08-26 08:28:35 +0000

    conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,6 +27,32 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33 ;
 let stringSomeNumber = String(someNumber);
-console.log(stringSomeNumber);
-console.log(typeof stringSomeNumber);
+//console.log(stringSomeNumber);
+//console.log(typeof stringSomeNumber);
 
+//*****************************    Operations    ******************************** */
+
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello" ;
+let str2 = " pintu" ;
+let str3 = str1 + str2 ;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
