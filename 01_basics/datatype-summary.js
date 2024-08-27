0256be20031d2673ae79f481f7e2da65ab3df672
Pintu9040 <pintuojha61@gmail.com>
2024-08-27 07:52:06 +0000

    datatype-summary

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,32 @@
+//# primitive
+
+// 7 types - string,number,boolean,null,undefined,,symbol,bigInt
+
+const score = 100
+const scoreValue = 100.3
+ 
+//const isLoggedIn = fulse 
+const outSideTemp = null
+let userEmail;
+
+//const sym1 = symbol('123');
+//const sym2 = symbol('123');
+//console.log(sym1 === sym2);
+
+// Reference (non- primitive)
+
+
+// Array,objects,functions
+const heros = ["saktiman","doga"]   //Array
+
+let myObj = {
+    name: "pintu",  //object
+    age: 29,
+}
+
+ const myFunction = function(){
+   console.log("hello world")    //function
+}
+
+ 
+console.log(typeof score);
