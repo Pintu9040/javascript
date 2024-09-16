96dbfe075f75a512ffb12ced1001d49a013af076
Pintu9040 <pintuojha61@gmail.com>
2024-09-16 07:10:14 +0000

    string

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,1 +1,25 @@
 const name = "pintu"
+const repoCount = 50
+
+ //console.log(name + repoCount);
+
+//console.log(`hello my name is ${name} and my repocount is ${repoCount}`);
+
+const gameName = new String('pintuth')
+
+//console.log(gameName[1]);
+
+//console.log(gameName.__proto__);
+
+//console.log(gameName.length);
+
+//console.log(gameName.toUpperCase());
+
+//console.log(gameName.charAt(3));
+
+const anotherString = gameName.slice(-5,2)
+console.log(anotherString);
+
+const newStringOne = "   pintu    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
