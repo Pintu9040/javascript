284ed83ea71d208752356d30a3e803a5d1204a4d
Pintu9040 <pintuojha61@gmail.com>
2024-09-16 07:43:53 +0000

    num and math

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,15 @@
+const score = 400
+console.log(score);
+ 
+const balanca = new Number(100)
+console.log(balanca);
+
+console.log(balanca.toString().length);
+console.log(balanca.toFixed(2));
+
+const otherNumber = 23.8945
+
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
